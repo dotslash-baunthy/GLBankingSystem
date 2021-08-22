010aad9c91bea679fa9ea233f36783b7524d2b31
Akshit Baunthiyal <akshit.baunthy@gmail.com>
2021-08-23 00:32:45 +0530

    Added logic to transfer to valid account

--- src/com/greatlearning/services/BankingService.java
+++ src/com/greatlearning/services/BankingService.java
@@ -6,9 +6,8 @@ import com.greatlearning.services.BankingService;
 import java.util.Scanner;
 
 public class BankingService {
-	
+
 	Scanner scn = new Scanner(System.in);
-	int bankBalance = 1000;
 
 	public void logOut() {
 		System.out.println("----------Thank you for banking with us----------");
@@ -29,7 +28,7 @@ public class BankingService {
 
 	public void withdraw(Customer customer) {
 		int amount;
-		System.out.println("Enter the amount to be withdrawn: ");
+		System.out.print("Enter the amount to be withdrawn: ");
 		amount = scn.nextInt();
 		if (customer.getBalance() >= amount) {
 			System.out.println("Amount withdrawn successfully!");
@@ -40,31 +39,41 @@ public class BankingService {
 		}
 	}
 
-	public void transfer(Customer customer) {
+	public void transfer(Customer loggedIncustomer, Customer otherCustomer) {
 		int amount;
 		int otp;
 		int otpGenerated;
 		int transferAccountNumber;
-		System.out.println("Enter the OTP: ");
+		System.out.print("Enter the OTP: ");
 		OTPService otpService = new OTPService();
 		otpGenerated = otpService.generateOTP();
-		System.out.println("Generated OTP is: ");
-		System.out.println(otpGenerated);
-		System.out.println("Enter the OTP: ");
+		System.out.println("Generated OTP is: " + otpGenerated);
+		System.out.print("Enter the OTP: ");
 		otp = scn.nextInt();
 		if (otp == otpGenerated) {
 			System.out.println("OTP validated successfully!");
-			System.out.println("Enter the amount to be transferred: ");
+			System.out.print("Enter the amount to be transferred: ");
 			amount = scn.nextInt();
-			System.out.println("Enter the account no. to be transferred to: ");
+			System.out.print("Enter the account no. to be transferred to: ");
 			transferAccountNumber = scn.nextInt();
-			if (customer.getBalance() >= amount) {
-				System.out.println("Amount was transferred!");
-				customer.setBalance(customer.getBalance() - amount);
-				System.out.println("Remaining balance is: " + customer.getBalance());
+
+			if (transferAccountNumber == otherCustomer.getBankAccountNo()) {
+				if (loggedIncustomer.getBalance() >= amount) {
+					otherCustomer.setBalance(otherCustomer.getBalance() + amount);
+					loggedIncustomer.setBalance(loggedIncustomer.getBalance() - amount);
+					System.out.println("Amount was transferred!");
+					System.out.println("Remaining balance is: " + loggedIncustomer.getBalance());
+				} else {
+					System.out.println("Insufficient balance!");
+				}
 			} else {
-				System.out.println("Insufficient balance!");
+				System.out.println("The recipient does not exist in our database");
 			}
 		}
+	}
+
+	public void checkBalance(Customer loggedInCustomer) {
+		System.out.println("Customer " + loggedInCustomer.getBankAccountNo() + " has a balance of "
+				+ loggedInCustomer.getBalance());
 	}
 }
